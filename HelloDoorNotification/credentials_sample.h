d25b04f9d8db00c508491b4550f8192434f66375
Marko Oklobdžija <marko.n.oklobdzija@gmail.com>
2019-08-11 13:21:19 +0200

    Apply Clang formatting on sample cred. file

--- HelloDoorNotification/credentials_sample.h
+++ HelloDoorNotification/credentials_sample.h
@@ -1,5 +1,5 @@
 #pragma once
 
-static const char* const resource = "/trigger/esp32_reading/with/key/<key>";
-static const char* const ssid = "<wifi_ssid>";
-static const char* const pass = "<wifi_pass>";
+static const char *const resource = "/trigger/esp32_reading/with/key/<key>";
+static const char *const ssid = "<wifi_ssid>";
+static const char *const pass = "<wifi_pass>";
